553119596bab018936904cd262337b12bc4087aa
strau0106 <strau0106@gmail.com>
2023-04-23 20:46:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 setup(
     name='pre_commit_pytype',
     version='1.1.0',
-    install_requires=['pytype==2022.4.26'],
+    install_requires=['pytype==2023.04.18'],
 )
